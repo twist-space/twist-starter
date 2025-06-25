8aad45be2572cd24efba867f534e2eeb3a79c32a
razzh <razzhavenir@163.com>
2025-06-25 15:34:46 +0800

    init

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import antfu from '@antfu/eslint-config';
+
+export default antfu({
+  stylistic: {
+    semi: true,
+  },
+  type: 'lib',
+  rules: {
+    'no-useless-return': 'off',
+    'antfu/if-newline': 'off',
+    'style/quote-props': 'off',
+    'react/prefer-destructuring-assignment': 'off',
+    'style/brace-style': 'off',
+    'react/no-unstable-context-value': 'off',
+    'react/no-children-for-each': 'off',
+    'regexp/no-unused-capturing-group': 'off',
+    'test/consistent-test-it': 'off',
+    'node/prefer-global/process': 'off',
+  },
+  ignores: ['.github/*'],
+});
